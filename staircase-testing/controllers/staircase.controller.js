b5d74d46db1d81e087e59e39176023cec32a0b8f
chinmaya <cchinmaya@sapient.com>
2018-08-01 17:46:35 +0100

    Added some negative unitcase

--- staircase-testing/controllers/staircase.controller.js
+++ staircase-testing/controllers/staircase.controller.js
@@ -1,21 +1,31 @@
+
 function staircasecontroller() {
 
-    function stepscount(totalsteps) {
-        if (totalsteps <= 1)
-            return totalsteps;
-        if (totalsteps < 2)
-            return totalsteps;
-        if (totalsteps ==2)
-            return totalsteps;
 
-        var NoOfWays=  (totalsteps - 1) + (totalsteps - 2);
-        return NoOfWays;
+    function isInteger(num) {
+        return num % 1 === 0;
+    }
+
+    function fibonacci(steps) {
+        if (steps <= 1) {
+            return steps;
+        }
+        return fibonacci(steps - 1) + fibonacci(steps - 2);
+    }
+
+    function stepscount(totalsteps) {
+
+        if (!isInteger(totalsteps) || totalsteps < 0)
+            return 0;
+
+        return fibonacci(totalsteps + 1);
     }
 
     return {
         stepscount
     }
-
 }
 
 module.exports = staircasecontroller();
+
+
--- staircase-testing/test/controllers/staircase.controller.spec.js
+++ staircase-testing/test/controllers/staircase.controller.spec.js
@@ -5,45 +5,59 @@ describe('staircasecontroller', function () {
 
     describe('stepscount', function(){
 
-        it('should return true if ways of path is valid first stiarcase ', function(){
-            var count = staircasecontroller.stepscount('0')
-            console.log('count of possible ways to reach top  = '+count);
-            assert.equal(count, 0);
-        });
-
-        it('should return true if ways of path is valid second stiarcase ', function(){
-            var count = staircasecontroller.stepscount('1')
-            console.log('count of possible ways to reach top  = '+count);
+        it('should return true if ways of path is valid no stiarcase ', function(){
+            var count = staircasecontroller.stepscount(0);
+            console.log('Count of possible ways to reach top  = '+count);
             assert.equal(count, 1);
         });
 
-        it('should return true if ways of path is valid third stiarcase ', function(){
-            var count = staircasecontroller.stepscount('2')
-            console.log('count of possible ways to reach top  = '+count);
+        it('should return true if ways of path is valid first stiarcase ', function(){
+            var count = staircasecontroller.stepscount(1);
+            console.log('Count of possible ways to reach top  = '+count);
+            assert.equal(count, 1);
+        });
+
+        it('should return true if ways of path is valid second stiarcase ', function(){
+            var count = staircasecontroller.stepscount(2);
+            console.log('Count of possible ways to reach top  = '+count);
             assert.equal(count, 2);
         });
 
+        it('should return true if ways of path is valid third stiarcase ', function(){
+            var count = staircasecontroller.stepscount(3);
+            console.log('Count of possible ways to reach top  = '+count);
+            assert.equal(count, 3);
+        });
+
+        it('should return true if ways of path is valid 4th stiarcase ', function(){
+            var count = staircasecontroller.stepscount(4);
+            console.log('Count of possible ways to reach top  = '+count);
+            assert.equal(count, 5);
+        });
+
         it('should return true if ways of path is valid 5th stiarcase ', function(){
-            var count = staircasecontroller.stepscount('5')
-            console.log('count of possible ways to reach top  = '+count);
-            assert.equal(count, 7);
+            var count = staircasecontroller.stepscount(5);
+            console.log('Count of possible ways to reach top  = '+count);
+            assert.equal(count, 8);
         });
 
-        it('should return true if ways of path is valid with total 20th steps ', function(){
-            var count = staircasecontroller.stepscount('20')
-            console.log('count of possible ways to reach top  = '+count);
-            assert.equal(count, 37);
+        it('should return true if ways of path is valid 6th stiarcase ', function(){
+            var count = staircasecontroller.stepscount(6);
+            console.log('Count of possible ways to reach top  = '+count);
+            assert.equal(count, 13);
         });
 
-        it('should not return true value for negative value stiarcase', function(){
-            var count = staircasecontroller.stepscount('-1')
-            console.log('count of possible ways to reach top  = '+count);
-            assert.equal(count, -1);
+
+        it('should return false value for negative value stiarcase', function(){
+            var count = staircasecontroller.stepscount(-4);
+            console.log('Count of possible ways to reach top  = '+count);
+            assert.equal(count, 0);
         });
 
-        it('should not return true value for string value stiarcase ', function(){
-            var count = staircasecontroller.stepscount('chinmaya')
-            console.log('count of possible ways to reach top = '+count);
+        it('should return false value for string value stiarcase ', function(){
+            var count = staircasecontroller.stepscount('chinmaya');
+            console.log('Count of possible ways to reach top = '+count);
+            assert.equal(count, 0);
         });
 
     });
